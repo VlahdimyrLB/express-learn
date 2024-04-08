6d4e40664918bcc5566c2b9158712158d70ede72
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-04-09 02:32:00 +0800

    Finished express basics

--- final/03-express-basics.js
+++ final/03-express-basics.js
@@ -1,22 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require("express");
 
-app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
-})
+//invoke the express function
+const app = express();
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
-})
+// the callback will invoke everytime the user perform get request on the root '/'
+app.get("/", (req, res) => {
+  console.log("User visits the homepage");
+  res.status(200).send("HOME PAGE");
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
-})
+app.get("/about", (req, res) => {
+  console.log("User visits the about");
+  res.status(200).send("About Page");
+});
+
+// all handles all http requests
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>PAGE NOT FOUND</h1>");
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
-})
+  console.log("Server is listening on port 5000...");
+});
 
 // app.get
 // app.post
--- final/04-express-app.js
+++ final/04-express-app.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const app = express()
+// will make all resources from publice available
+// setup static and middleware(app.use)
+app.use(express.static("./public"));
 
-// setup static and middleware
-app.use(express.static('./public'))
+app.get("/", (req, res) => {
+  // we sendFile since we are responding to user request
+  // we can also use path.join
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+app.get("/about", (req, res) => {
+  console.log("User visits the about");
+  res.status(200).send("About Page");
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+// all handles all http requests
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>PAGE NOT FOUND</h1>");
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000...");
+});
