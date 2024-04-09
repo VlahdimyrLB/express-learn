6938ea0744b5fe70e82628a7944dafde8726ed8f
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-04-10 02:23:52 +0800

    Finished methods

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 // parse json
 app.use(express.json());
 
+// GET
 app.get("/api/people", (req, res) => {
   res.status(200).json({ sucess: true, data: people });
 });
@@ -28,9 +29,54 @@ app.post("/api/people", (req, res) => {
   if (!name) {
     res
       .status(400)
-      .json({ success: false, msg: "PLease provide a name value" });
+      .json({ success: false, msg: "Please provide a name value" });
   }
   res.status(201).json({ success: true, person: name });
+});
+
+// POST
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, msg: "Please provide a name value" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
+// PUT
+app.put("/api/postman/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    res.status(400).json({ success: false, msg: `No person with id: ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+// DELETE
+app.delete("/api/postman/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    res
+      .status(400)
+      .json({ success: false, msg: `No person with id: ${req.params.id}` });
+  }
+
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+
+  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
